8176c5889c54e8f7d0d67e7db826c98fe58aaa5c
RamuMakkena <98073031+RamuMakkena@users.noreply.github.com>
2022-03-29 19:28:10 -0500

    Update weatherapi.js

--- assets/js/weatherapi.js
+++ assets/js/weatherapi.js
@@ -1,4 +1,4 @@
-levar weatherEndpoint='http://api.openweathermap.org/data/2.5/onecall?exclude=current,minutely,hourly,alerts&APPID=f65a1aee858b1d7af065cd4b5b972ce8&units=metric&';
+var weatherEndpoint='http://api.openweathermap.org/data/2.5/onecall?exclude=current,minutely,hourly,alerts&APPID=f65a1aee858b1d7af065cd4b5b972ce8&units=metric&';
 var geoLocationEndPOint = 'http://api.openweathermap.org/geo/1.0/direct?APPID=f65a1aee858b1d7af065cd4b5b972ce8&';
 
 //SUbmitting weather api
